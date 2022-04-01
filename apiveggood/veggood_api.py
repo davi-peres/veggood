c1d93a8c631350af6da4be3da54c890fb744d17a
alicemdd <82115003+alicemdd@users.noreply.github.com>
2022-04-01 15:47:09 -0300

    Add files via upload
    Integração front com back:
    
    Páginas html precisam estar na pasta "templates", para estar de acordo com os padrões do flask;
    Imagens e arquivos css precisam ser armazenadas na pasta static;
    Nomes de arquivos foram modificados para a substituição de espaços por _, e remoção de acentos;
    Correção de referencias às imagens que apontavam para o caminho antigo do projeto, agora ficando em padrão Flask.

---
+++ apiveggood/veggood_api.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/post", methods=['POST'])
+
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
+
+
+@app.route("/quemsomos")
+def quemsomos():
+    return render_template("quemsomos.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
