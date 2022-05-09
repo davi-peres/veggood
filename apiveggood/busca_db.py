d7a890ba2ef5a5a974e7d7203934f38375238725
alicemdd <82115003+alicemdd@users.noreply.github.com>
2022-05-09 16:08:26 -0300

    Add files via upload

--- apiveggood/busca_db.py
+++ apiveggood/busca_db.py
@@ -3,8 +3,9 @@ import json
 
 def busca_json(query):
     restaurante_db = open("restaurantes.txt","r")
-    parsed_json = json.loads(restaurante_db.read())
+    parsed_json = json.loads(restaurante_db.read(),  strict=False)
     for i in parsed_json['restaurantes']:
-        if str(query).lower() in str(i['nome']).lower():
+        if str(query).lower() in str(i['produtos']).lower() or str(query).lower() in str(i['nome'].lower()):
             return i['url']
     return None
+
--- apiveggood/veggood_api.py
+++ apiveggood/veggood_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
 from flask_session import Session
 from busca_db import busca_json
 
@@ -8,13 +8,33 @@ query = None
 
 
 class Novocad:
-    def __init__(self, nome, email, senha):
-        self.nome = nome
+    def __init__(self, nomesobrenome, email, telefone, cep, endereco, numero, cidade, complemento):
+        self.nomesobrenome = nomesobrenome
         self.email = email
-        self.senha = senha
+        self.telefone = telefone
+        self.cep = cep
+        self.endereco = endereco
+        self.numero = numero
+        self.cidade = cidade
+        self.complemento = complemento
+
     def registrar(self):
         db = open("users.txt", "a+")
-        db.write(f"user: {self.nome}, email: {self.email}, senha: {self.senha}\n")
+        db.write(f"user: {self.nomesobrenome}, email: {self.email}, telefone: {self.telefone}, cep: {self.cep}"
+                 f", endereco: {self.endereco}, numero: {self.numero}, cidade: {self.cidade}, complemento: {self.complemento}\n")
+
+
+class Mensagem:
+    def __init__(self, nomesobrenome, email, telefone, mensagem):
+        self.nomesobrenome = nomesobrenome
+        self.email = email
+        self.telefone = telefone
+        self.mensagem = mensagem
+
+    def registrar_msg(self):
+        db = open("mensagem.txt", "a+")
+        db.write(f"Nome e sobrenome: {self.nomesobrenome}, Email: {self.email}, "
+                 f"Telefone: {self.telefone}\n, Mensagem: {self.mensagem}\n")
 
 
 app = Flask(__name__)
@@ -22,7 +42,7 @@ app = Flask(__name__)
 
 @app.route("/cadastro")
 def cadastrar():
-    return render_template("cadastro.html", titulo="Cadastro de novo usuário")
+    return render_template("cadastro.html")
 
 
 @app.route("/retornocad")
@@ -30,12 +50,17 @@ def retornarcad():
     return render_template("retornocad.html")
 
 
-@app.route("/usarionovo", methods=['POST'])    #rota de processamento
+@app.route("/usarionovo", methods=['POST'])    #rota de processamento de cadastro
 def usarionovo():
-    nome = request.form['nome']
+    nomesobrenome = request.form['nomesobrenome']
     email = request.form['email']
-    senha = request.form['senha']
-    novocad = Novocad(nome, email, senha)
+    telefone = request.form['telefone']
+    cep = request.form['cep']
+    endereco = request.form['endereco']
+    numero = request.form['numero']
+    cidade = request.form['cidade']
+    complemento = request.form['complemento']
+    novocad = Novocad(nomesobrenome, email, telefone, cep, endereco, numero, cidade, complemento)
     novocad.registrar()
     return redirect('retornocad')
 
@@ -55,6 +80,18 @@ def contato():
     return render_template("contato.html")
 
 
+@app.route("/msgenviada", methods=['POST']) #rota de processamento mensagem
+def envia_msg():
+    nome = request.form['nomesobrenome']
+    email = request.form['email']
+    telefone = request.form['telefone']
+    mensagem = request.form['mensagem']
+    msg = Mensagem(nome, email, telefone, mensagem)
+    msg.registrar_msg()
+    flash("Mensagem enviada com sucesso!")
+    return redirect('contato')
+
+
 @app.route("/resultadobusca", methods=['POST'])
 def resultado():
     query = request.form['pesquisa']
@@ -65,7 +102,6 @@ def resultado():
     return render_template("resultados.html", data="Resultado não encontrado.")
 
 
-
 @app.route("/link", methods=['GET'])  #rota redirecionamento
 def link():
     url = session.get('url', None)
@@ -74,6 +110,21 @@ def link():
         return redirect(url_query)
     else:
         return render_template("index.html")
+
+
+@app.route("/clubevegano")
+def clube_vegano():
+    return render_template("clube-vegano.html")
+
+
+@app.route("/veggiesabor")
+def veggie_sabor():
+    return render_template("veggie-sabor.html")
+
+
+@app.route("/vidavegana")
+def vida_vegana():
+    return render_template("vida-vegana.html")
 
 
 if __name__ == "__main__":
